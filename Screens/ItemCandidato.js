67c1d371376a7e212b14e665950968d29f08fdd9
geovibelen <geovibelen2@hotmail.com>
2020-09-19 22:20:06 -0500

    Item Candidato

--- Screens/ItemCandidato.js
+++ Screens/ItemCandidato.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Image, Text, FlatList } from "react-native";
-import { Input, Button, Avatar } from "react-native-elements";
+import { Input, Button, Avatar, ListItem } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome5";
 
 export class ItemCandidato extends Component {
   render() {
     return (
       <View style={styles.fila}>
+        <Avatar
+          rounded
+          source={{
+            uri:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTttVuZJb7dABxNTs-y5MHOYfRd1vUM42V3-g&usqp=CAU",
+          }}
+        />
         <Text style={styles.texto}>
           {this.props.pCandidato.nombre} {this.props.pCandidato.apellido}
         </Text>
@@ -26,7 +33,7 @@ const styles = StyleSheet.create({
   },
   texto: {
     fontSize: 20,
-    textAlign: "center",
+
     fontWeight: "bold",
   },
 });
